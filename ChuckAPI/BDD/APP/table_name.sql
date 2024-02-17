3018515592c23e6ca36e50ef748a9f1163d10b50
ThéoL <99662062+TheoLugagne@users.noreply.github.com>
2024-02-17 10:56:39 +0100

    Delete ChuckAPI directory

--- ChuckAPI/BDD/APP/table_name.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `chuckn_facts` (
-                                `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                                `phrase` text CHARACTER SET latin1 NOT NULL,
-                                `vote` int(11) DEFAULT NULL,
-                                `date_ajout` datetime DEFAULT NULL,
-                                `date_modif` datetime DEFAULT NULL,
-                                `faute` tinyint(1) DEFAULT NULL,
-                                `signalement` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
